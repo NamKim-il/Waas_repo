309aec83ec375d6f8cc09dc3fefa99f4c9bf14f2
NamKim-il <namil000208@gmail.com>
2019-12-26 16:14:57 +0900

    '제출'

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const todoRouter = require('./routes/todo');
+const formRouter = require('./routes/form');
+const mimeRouter = require('./routes/mime');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/todo', todoRouter);
+app.use('/form', formRouter);
+app.use('/mime', mimeRouter);
+
+// 404 Not Found
+app.use(function(req, res, next) {
+  res.status(404).send("404 Not Found");
+});
+
+module.exports = app;
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+``` markdown
+### 12월 23일
+* todo list의 제목을 "HelloTODO"로 하기 위해서는 localhost:3000/todo?title=HelloTODO 로 접근해야 함
+```
